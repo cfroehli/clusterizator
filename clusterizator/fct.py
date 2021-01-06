185fa77807d5893faabb47ed3c53703f67e6eeae
Cyrille Froehlich <cfroehli@users.noreply.github.com>
2021-01-06 10:00:43 +0900

    Create a dataset to play with

--- clusterizator/fct.py
+++ clusterizator/fct.py
@@ -1,4 +1,30 @@
+import random
 import numpy as np
+
+class ClusterPoint:
+    def __repr__(self):
+        return "XY: {} / Cluster: {}".format(self.coordinates, self.cluster)
+
+    @property
+    def coordinates(self):
+        return self._coordinates
+
+    def __init__(self, coordinates, cluster):
+        self._coordinates = coordinates
+        self.cluster = cluster
+
+# Fake some data by grouping pts around a few areas
+centers = ([6, 4], [-8, -3], [-5, 5])
+n_cluster = len(centers)
+clusters_size = [4] * n_cluster
+
+def create_dataset(centers, clusters_size):
+    dataset = np.repeat([np.array(c, dtype=float) for c in centers], clusters_size, axis=0)
+    dataset += np.random.randn(*dataset.shape)
+    return dataset
+
+dataset = create_dataset(centers, clusters_size)
+print(dataset)
 
 # Get some points (more or less grouped in blobs)
 # Random assign to clusters
