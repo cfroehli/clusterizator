0a32fc6b9434799100f986470a63289df88f63e3
Cyrille Froehlich <cfroehli@users.noreply.github.com>
2021-01-07 00:38:43 +0900

    Add linter/coverage tools

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,18 +1,17 @@
 import itertools
-import pytest
 
 import numpy as np
+import clusterizator as Ctor
 
-from clusterizator import *
 
 def test_cluster():
     np.random.seed(3)
     cluster_size = 200
-    dataset, n_cluster = create_3blobs_dataset(cluster_size)
-    clusterizator = Clusterizator(dataset)
+    dataset, n_cluster = Ctor.create_3blobs_dataset(cluster_size)
+    clusterizator = Ctor.Clusterizator(dataset)
     results = clusterizator.run(n_cluster, max_epochs=50)
     ids = results['clusters_ids']
-    ids_groups = [ len(list(g)) for k, g in itertools.groupby(ids) ]
+    ids_groups = [len(list(g)) for k, g in itertools.groupby(ids)]
     assert len(ids_groups) == n_cluster
     for group_size in ids_groups:
         assert group_size == cluster_size
