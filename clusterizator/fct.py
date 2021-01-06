a25e9315a1fc4b45d2fa56273fcdfb7e4429ada2
Cyrille Froehlich <cfroehli@users.noreply.github.com>
2021-01-06 09:53:48 +0900

    Planning ...

--- clusterizator/fct.py
+++ clusterizator/fct.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+# Get some points (more or less grouped in blobs)
+# Random assign to clusters
+# For each cluster, find the centroid (average points)
+# For each point, check distance to centroids and assign to nearest
+# Rebuild clusters, if any cluster empty grap a point from another cluster
+# Repeat until stable or bored
